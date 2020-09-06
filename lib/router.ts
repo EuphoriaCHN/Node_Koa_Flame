e2d4b687a4bb32eb377f022525ac24c2bc1e0f8b
EuphoriaCHN <879969355@qq.com>
2020-09-07 01:00:48 +0800

    stash

--- lib/router.ts
+++ lib/router.ts
@@ -1,9 +1,27 @@
 import KoaRouter from 'koa-router';
+import EuphoriaNode from '../index';
+import { ExtendableContext, Next } from 'koa';
 
 namespace NRouter {
+  export type HTTP_METHODS = 'get' | 'post' | 'head' | 'delete' | 'link' | 'unlink' | 'put' | 'options' | 'patch' | 'all';
+
+  export type Routes = {
+    method?: HTTP_METHODS,
+    to: <T = any>(ctx: ExtendableContext, next?: Next) => Promise<T>
+  };
+
+  type RouterOptions = {
+    app: EuphoriaNode,
+    routes: {}
+  };
+
   export class Router extends KoaRouter {
-    constructor() {
+    constructor(options?: RouterOptions) {
       super();
+
+      if (!options) {
+        return;
+      }
     }
   }
 }
